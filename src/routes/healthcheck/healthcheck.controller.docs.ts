1509ada563a3c4ffa5af9976f6650420d5328e94
Chase Sigmon <csigmon4832@gmail.com>
2024-06-27 19:53:44 -0400

    updated description

--- src/routes/healthcheck/healthcheck.controller.docs.ts
+++ src/routes/healthcheck/healthcheck.controller.docs.ts
@@ -1,28 +1,28 @@
 import {
-    applyDecorators,
-    BadRequestException,
-    Version,
-    VERSION_NEUTRAL,
+  applyDecorators,
+  BadRequestException,
+  Version,
+  VERSION_NEUTRAL,
 } from '@nestjs/common';
 import {
-    ApiBadRequestResponse,
-    ApiOkResponse,
-    ApiOperation,
-    ApiTags,
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
 } from '@nestjs/swagger';
 
 import { HealthCheckResponse } from './healthcheck.model';
 
 export const GetHealthcheckDocs = () =>
-    applyDecorators(
-        Version(VERSION_NEUTRAL),
-        ApiTags('healthcheck'),
-        ApiOperation({
-            summary: 'Healthcheck',
-            description: 'Check the health of the mobile-food-tracker',
-        }),
-        ApiOkResponse({ type: HealthCheckResponse }),
-        ApiBadRequestResponse({
-            description: new BadRequestException().message,
-        })
-    );
+  applyDecorators(
+    Version(VERSION_NEUTRAL),
+    ApiTags('healthcheck'),
+    ApiOperation({
+      summary: 'Healthcheck',
+      description: 'Check the health of the Candy Store API.',
+    }),
+    ApiOkResponse({ type: HealthCheckResponse }),
+    ApiBadRequestResponse({
+      description: new BadRequestException().message,
+    }),
+  );
